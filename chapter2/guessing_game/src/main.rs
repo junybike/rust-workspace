b59c0b05f27baec2cec2c4fb81eafa81f11f3ae4
Jason Lee <79300253+junybike@users.noreply.github.com>
2025-01-07 21:59:31 -0700

    Finished chapter 2 comments

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -5,7 +5,8 @@ use std::cmp::Ordering;
 fn main() 
 {
     println!("Guess the number!");
-
+    
+    // Secret number. Has a number type (1 ~ 100)
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret}");
     
@@ -21,20 +22,22 @@ fn main()
             // take whatever user types into std input and append that into a string (no overwriting its contents)
             // &: reference. No needing to copy 'guess' data into memory multiple times   
             .read_line(&mut guess)      
-            .expect("Failed to read");
+            .expect("Failed to read");  // expect crashes the program and prints that message
 
+        // Check if the input guess is an integer. Trim eliminates '\n'. Parse converts a string to another type and returns either 'Ok' or 'Err'
+        // match handles errors. Has 2 arms here: Ok and Err.
         let guess: u32 = match guess.trim().parse()
         {
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(num) => num,     // Input is an integer
+            Err(_) => continue, // Input is not an integer. Receive a new input
         };
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret)
+        match guess.cmp(&secret)    // The cmp function returns one of the 3 value: Ordering::Less, Ordering::Greater, Ordering::Equal.
         {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {println!("You win!"); break;}
+            Ordering::Less => println!("Too small!"),           // Case: the guess is less than the secret number
+            Ordering::Greater => println!("Too big!"),          // Case: the guess is larger than the secret number
+            Ordering::Equal => {println!("You win!"); break;}   // Case: the guess is correct. End the loop
         }
     }
 }
