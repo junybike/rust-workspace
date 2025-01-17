eb69b6eb774a54b0d3157de23f928db819d3cf04
Jason Lee <79300253+junybike@users.noreply.github.com>
2025-01-17 15:58:28 -0800

    Add files via upload

---
+++ chapter14/crates/src/lib.rs
@@ -0,0 +1,10 @@
+//! # My Crate
+//! 
+//! `my_crate` is a collection of utilites to make performing certain
+//! calculations more convenient.
+/// Adds one to the number given
+pub fn add_one(x: i32) -> i32
+{
+    x + 1
+}
+
---
+++ chapter14/pic/src/lib.rs
@@ -0,0 +1,39 @@
+//! # Art
+//! 
+//! A library for modeling artistic concepts.
+
+// pub use statements to re-export items
+// Creates additional documentation on front page
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+pub mod kinds
+{
+    /// The primary colors according to the RYB color model
+    pub enum PrimaryColor
+    {
+        Red,
+        Yellow,
+        Blue,
+    }
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor
+    {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils
+{
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor
+    {
+        // implementation
+    }
+}
